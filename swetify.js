17dfe8e9119818913e21a4e3f3171cce69d438b4
Shwet Khatri <56475750+ShwetKhatri2001@users.noreply.github.com>
2020-10-31 00:31:34 +0530

    Update swetify.js

--- swetify.js
+++ swetify.js
@@ -34,7 +34,7 @@ const arijitsongs=[
 
 const nehasongs=[
 {name : "Dilbar",title : "Dilbar",artist : "Neha Kakkar, Dhvani Bhanushali, Ikka, Tanishk Bagchi"},
-{ name: "TuhiYaarMera",title : "Tu Hi Yaar Mera",artist : "Neha Kakkar, Arijit Singh, Rochak Kohli"},
+{name: "TuHiYaarMera",title : "Tu Hi Yaar Mera",artist : "Neha Kakkar, Arijit Singh, Rochak Kohli"},
 {name: "YaadPiyaKiAaneLagi",title : "Yaad Piya Ki Aane Lagi",artist : "Neha Kakkar, Tanishk Bagchi, Lalit Sen"},
 {name: "Garmi",title : "Garmi",artist : "Neha Kakkar, Badshah"},
 {name: "LaLaLa",title : "La La La",artist : "Neha Kakkar, Arjun Kanungo"},
