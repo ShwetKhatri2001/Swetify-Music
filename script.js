ca6e1b0b461343d6cfbf436527d525fb21a37ab5
Mrudula1205 <mrudula12523@gmail.com>
2023-10-22 21:20:23 +0400

    changes

---
+++ script.js
@@ -0,0 +1,48 @@
+const initSlider = () => {
+  
+    const imageList = document.querySelector(".slider-wrapper .center_div");
+    const slideButtons = document.querySelectorAll(".slider-wrapper .slide-button");
+    
+    
+
+    slideButtons.forEach(button => {
+        button.addEventListener("click",()=>{
+            const direction = button.id === "prev-slide"? -1: 1;
+            const scrollAmount = imageList.clientWidth*direction;
+            imageList.scrollBy({left:scrollAmount, behaviour:"smooth"});
+        })
+    })
+
+    const handleSlideButtons = () => {
+        slideButtons[0].style.display = imageList.scrollLeft <= 0? "none" : "block";
+        slideButtons[1].style.display = imageList.scrollLeft >= maxScrollLeft? "none" : "block";
+    }
+
+    
+
+    imageList.addEventListener("scroll", () => {
+        handleSlideButtons();
+        
+    });
+   
+}
+
+window.addEventListener("load",initSlider);
+
+/*const initSlider = () => {
+  
+    const imageList = document.querySelector(".slider-wrapper .center_div");
+    const slideButtons = document.querySelectorAll(".slider-wrapper .slide-button");
+
+    slideButtons.forEach(button => {
+        button.addEventListener("click",()=>{
+            const direction = button.id === "prev-slide"? -1: 1;
+            const scrollAmount = imageList.clientWidth*direction;
+            imageList.scrollBy({left:scrollAmount, behaviour:"smooth"});
+        })
+    })
+
+   
+}
+
+window.addEventListener("load",initSlider);*/
