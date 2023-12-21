7eccf57b191507088469fb4c0f2c06822a4e2f70
Sambit <thewhitedevil@32gmail.com>
2023-12-21 21:33:00 +0530

    Added Scroll-Reveal effect

---
+++ scroll-reveal.js
@@ -0,0 +1,42 @@
+//Insitialised the Scroll-Reveal component
+
+ScrollReveal({
+	reset: true,
+	distance: "40px",
+	duration: 2000,
+});
+
+// Customisation of Scroll Reveal rules for different classes
+
+ScrollReveal().reveal(
+	".navbar-header,.lg-display,.artist-container,.welcome-text",
+	{
+		delay: 200,
+		origin: "top",
+	}
+);
+ScrollReveal().reveal(".container,.btn", {
+	delay: 200,
+	origin: "bottom",
+	distance: "60px",
+});
+ScrollReveal().reveal("#holly", {
+	delay: 400,
+	origin: "left",
+	distance: "70px",
+});
+ScrollReveal().reveal(".footer-text", {
+	delay: 500,
+	origin: "left",
+	distance: "70px",
+});
+ScrollReveal().reveal("#bolly", {
+	delay: 400,
+	origin: "right",
+	distance: "70px",
+});
+ScrollReveal().reveal(".footer-links", {
+	delay: 500,
+	origin: "right",
+	distance: "70px",
+});
